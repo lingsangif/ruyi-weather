abf05135584d5cdc9ff89b9ef023cd8aced962a1
lingsangif <858078350@qq.com>
2026-06-15 10:58:19 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "ruyi-v2";
+const ASSETS = [
+  "/",
+  "index.html",
+  "css/style.css",
+  "js/app.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
